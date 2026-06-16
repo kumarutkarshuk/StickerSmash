8ffe1681e147bdab4191ff9d707f967a0f922b33
kumarutkarshuk <kumarutkarshuk18@gmail.com>
2026-06-17 00:27:47 +0530

    build a screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
-import { Link } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary"/>
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -17,14 +23,43 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
+
+// import { Link } from "expo-router";
+// import { Text, View, StyleSheet } from "react-native";
+
+// export default function Index() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>Home Screen</Text>
+//       <Link href="/about" style={styles.button}>
+//         Go to About screen
+//       </Link>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#25292e",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   text: {
+//     color: "#fff",
+//   },
+//   button: {
+//     fontSize: 20,
+//     textDecorationLine: "underline",
+//     color: "#fff",
+//   },
+// });
