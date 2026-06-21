44eef9fa94a45b1045e0a30979cea48bbb653195
kumarutkarshuk <kumarutkarshuk18@gmail.com>
2026-06-21 12:54:46 +0530

    implement image selection, emoji stickers, and save functionality; add new components for emoji picker and button interactions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      {/* <Stack.Screen name="index" options={{ title: "Home" }} />
+    <>
+      <Stack>
+        {/* <Stack.Screen name="index" options={{ title: "Home" }} />
       <Stack.Screen name="about" options={{ title: "About" }} /> */}
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
